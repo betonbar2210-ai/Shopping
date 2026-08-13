dde585b315bcecd3495adf005cffc2a98fe1960a
Gluyki <betonbar2210@gmail.com>
2026-08-13 20:18:21 +0300

    update setting.py

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,10 @@
+import os
+
+from dotenv import load_dotenv
 from pathlib import Path
 
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,9 +55,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DATABASE_NAME'), 
+        'USER': os.getenv('DATABASE_USER'), 
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'), 
+        'HOST': os.getenv('DATABASE_HOST'), 
+        'PORT': os.getenv('DATABASE_PORT', default='5432'), 
+    } 
 }
 
 AUTH_PASSWORD_VALIDATORS = [
