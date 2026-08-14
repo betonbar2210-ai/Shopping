cfbbb1178d0cb53fca088395ced736d8de018c64
Gluyki <betonbar2210@gmail.com>
2026-08-14 12:42:14 +0300

    исправлено блэком

--- catalog/management/commands/add_product.py
+++ catalog/management/commands/add_product.py
@@ -3,18 +3,32 @@ from catalog.models import Product, Category
 
 
 class Command(BaseCommand):
-    help = 'Add a new product'
+    help = "Add a new product"
 
     def handle(self, *args, **options):
-        category, _ = Category.objects.get_or_create(name='Забор', description='Забор железобетонный')
+        category, _ = Category.objects.get_or_create(
+            name="Забор", description="Забор железобетонный"
+        )
         products = [
-            {'name':'ПО-2м', 'description':'Забор 2500/3000 с фартуком', 'price': '12800', 'category': category},
-            {'name':'ПО-2', 'description':'Забор 3000/3000 без фартука', 'price': '12100', 'category' :category},
-            ]
+            {
+                "name": "ПО-2м",
+                "description": "Забор 2500/3000 с фартуком",
+                "price": "12800",
+                "category": category,
+            },
+            {
+                "name": "ПО-2",
+                "description": "Забор 3000/3000 без фартука",
+                "price": "12100",
+                "category": category,
+            },
+        ]
 
         for prod in products:
             product, created = Product.objects.get_or_create(**prod)
             if created:
-                self.stdout.write(self.style.SUCCESS(f'{product.name} created successfully'))
+                self.stdout.write(
+                    self.style.SUCCESS(f"{product.name} created successfully")
+                )
             else:
-                self.stdout.write(self.style.SUCCESS(f'{product.name} already exists'))
+                self.stdout.write(self.style.SUCCESS(f"{product.name} already exists"))
--- catalog/management/commands/load_catalog.py
+++ catalog/management/commands/load_catalog.py
@@ -1,18 +1,17 @@
-
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 from catalog.models import Category, Product
 
+
 class Command(BaseCommand):
-    help = 'Удаляем продукты и категории, загрузить фикстуры'
+    help = "Удаляем продукты и категории, загрузить фикстуры"
 
     def handle(self, *args, **options):
         Product.objects.all().delete()
         Category.objects.all().delete()
-        self.stdout.write('Продукты и категории удалены')
+        self.stdout.write("Продукты и категории удалены")
 
-        self.stdout.write('Загрузка фикстур')
-        call_command('loaddata', 'catalog/fixtures/catalog_data.json')
+        self.stdout.write("Загрузка фикстур")
+        call_command("loaddata", "catalog/fixtures/catalog_data.json")
 
-        self.stdout.write(self.style.SUCCESS('Каталог успешно перезагружен!'))
-    
+        self.stdout.write(self.style.SUCCESS("Каталог успешно перезагружен!"))
